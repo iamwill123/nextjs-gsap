11c547cab7af7845a93109f42793507d982392f5
iamwill123 <williampeiyuan@gmail.com>
2021-07-29 15:04:24 -0400

    commented out

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps, router }) {
+	// * isRootPage => does nothing right now
 	const isRootPage = router.pathname === '/'
 	return <Component {...pageProps} />
 }
